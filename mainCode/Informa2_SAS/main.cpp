6a51c9772efe5c15f83be3061fd4cad7aebdf5b4
Miguel Martin <miguel.martinm@udea.edu.co>
2021-04-23 19:39:28 -0500

    Desarrollo de funcion para convertir valores decimales en binarios. Cada patron se almacena en un arreglo con elementos en representación decimal de cada conjunto de 8 bits. Se trata de implementar una funcion eraser, para poner la matriz de leds en LOW ya que cada que se ingresa un nuevo patrón, la columna más a la izquierda se queda encendida.

--- mainCode/Informa2_SAS/main.cpp
+++ mainCode/Informa2_SAS/main.cpp
@@ -15,13 +15,33 @@ const int SRCLK = 5 ;
 unsigned int digitalSignal[3] = {0, 1, 0};
 unsigned int puertos[3] = {SER, RCLK, SRCLK};
 
-//int byteI[] = { 0x7E, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x7E }; //I
-int charA[][8] = {{0,1,1,1,1,1,1,0}, {1,1,1,1,1,1,1,1}, {1,1,0,0,0,0,1,1}, {1,1,1,1,1,1,1,1}, {1,1,1,1,1,1,1,1},  {1,1,0,0,0,0,1,1}, {1,1,0,0,0,0,1,1}, {1,1,0,0,0,0,1,1}};
+int (*matrizLeds)[MAXCOLUMN] = new int[8][8];
+
+
+int patron[MAXROW][MAXCOLUMN];
+
+
+//int charA[]={65,126,255,195,255,255,195,195,195}; //A
+//int charI[]={73,255,255,24,24,24,24,255,255}; //I
+//int charE[]={69,255, 255, 192, 254, 254, 192, 255, 255}; //E
+//int charJ[]={74,3, 3, 3, 3, 3, 195, 255, 126}; // J
+
+
+int characters[][9] = { {65,126,255,195,255,255,195,195,195},        // A
+                        {73,255,255,24,24,24,24,255,255},            // I
+                        {69,255, 255, 192, 254, 254, 192, 255, 255}, // E
+                        {74,3, 3, 3, 3, 3, 195, 255, 126}            // J
+                      };
 
 // **************** FUNCTIONS PROTOTYPE ***************
 
 int verificacion(int [][MAXCOLUMN]);
-int imagen(int [][MAXCOLUMN]);
+int imagen(int [][MAXCOLUMN],int [][MAXCOLUMN]);
+
+
+//int decimalToBinary(int [], int (*p)[MAXCOLUMN]);
+int decimalToBinary(int []);
+
 void dataSER(int );
 void signalSRCLK();
 void signalRCLK();
@@ -32,7 +52,10 @@ void signalRCLK();
 
 
 int main()
-{
+{   //int b[] = {8,41};
+    //int arreglo[][3] = {{*b},{7,54},{35,15}};
+    //int patron[MAXROW][MAXCOLUMN];
+    //int (*ptrPatron)[MAXCOLUMN] = patron;
 
     /*  // Caracter a ASCCI a binario
     char caracter;
@@ -59,21 +82,63 @@ int main()
         cout << binaryChar[j];
     }*/
 
-     Inicialización de los puertos digitales
-    for(unsigned int i = 0 ; i < 3 ; i++ ){
-        pinMode( puertos[i] , OUTPUT );
-        digitalWrite( puertos[i] , 0 );
+//     Inicialización de los puertos digitales
+//    for(unsigned int i = 0 ; i < 3 ; i++ ){
+//        pinMode( puertos[i] , OUTPUT );
+//        digitalWrite( puertos[i] , 0 );
+//    }
+    char caracter;
+
+    cout << "Caracter: ";
+    cin >> caracter;
+
+    //cout << "Aquí: " << **(arreglo+1) << endl;
+
+    //cout << "Aquí" << *(*(characters+1)+3) ;
+    cout << "Decimal: " << **(characters+2) ;
+
+    for( int i = 0; i < 4 ; i++ ){
+//        if( int(caracter) == **(characters + i) ){
+//            decimalToBinary( *(characters+i), ptrPatron );
+//        }
+        if( caracter == **(characters+i) ){
+            //Rif( caracter == characters[i][0] ){
+            //out << "Valor: "<< characters[i][0];
+            //cout << "Bit: "<< characters[i+1][0];
+            //cout << "Bit: "<< charA[i+1];
+            cout << characters[i];
+            //decimalToBinary( characters[i], ptrPatron );
+
+            // Envía como parámetro el arreglo que esté en la posición actual
+            decimalToBinary( characters[i]);
+        }
     }
+/*
+    for( int i = 0; i < 1 ; i++ ){
+        if( int(caracter) == **(characters + i) ){
+            //decimalToBinary( *(characters+i), ptrPatron );
+            //decimalToBinary( *(characters+i));
+            //decimalToBinary( characters[i][0] );
+        }
+//        if( caracter == charA[0] ){
+//            //cout << "Valor: "<< characters[i][0];
+//            //cout << "Bit: "<< characters[i+1][0];
+//            //cout << "Decimal: "<< charA[i+1];
+//            //cout << characters[i];
+//            //decimalToBinary( characters[i], ptrPatron );
+//            decimalToBinary( charA);
+//        }
+    }*/
 
 
-    int matrizLeds[MAXROW][MAXCOLUMN];
+    //int matrizLeds[MAXROW][MAXCOLUMN];
     //int (*ptrMatrizLeds)[8][8];
 
     //ptrMatrizLeds = &matrizLeds;
 
-    verificacion(matrizLeds);
-
-
+//    verificacion(matrizLeds);
+//    imagen(ptrPatron, matrizLeds);
+    imagen(patron, matrizLeds);
 
     return 0;
 }
@@ -83,20 +148,98 @@ int main()
 // **************** FUNCTIONS DEFINITION ***************
 // *****************************************************
 
-int verificacion(int matriz[][8]){
+//int patron[MAXROW][MAXCOLUMN];
+//int charA[]={65,126,255,195,255,255,195,195,195};
+
+
+int eraser(int matriz[][8]){
 
     // Rellenado de toda la matríz con valores igual a 1
     for(int row = 0; row < MAXROW ; row++){
         for(int column = 0; column < MAXCOLUMN ; column++){
-            matriz[row][column] = 1;
-            //cout << " " << matriz[row][column];
-            dataSER(matriz[row][column]);
+            matriz[row][column] = 0;
+            cout << " " << matriz[row][column];
+            //dataSER(matriz[row][column]);
             signalSRCLK();
             signalRCLK();
         }
         //cout << endl;
     }
 
+    //dataSER(matriz[]);
+
+
+    return 0;
+}
+
+
+//int decimalToBinary( int arrayPatron[], int (*patron)[8] ){
+int decimalToBinary( int arrayPatron[] ){
+
+    int bit, decimal=0;
+
+
+    //cout << "\nChar in function: " << arrayPatron[0];
+    //cout << "\nDecimal in Function: " << arrayPatron[1];
+    for(int j = 0; j < 8; j++){
+        bool biteDivide = true;
+        int k=8;
+        //for(int k = 7; k >= 0; k--){
+            //division =  arrayPatron[j+1] / 2;
+            decimal =  arrayPatron[j+1];
+            //cout << "\nDecimal in Function: " << decimal;
+            //bit = arrayPatron[j+1] % 2;
+            //bit = decimal % 2;
+            while(biteDivide){
+                k--;
+                //division =  *(arrayPatron + (j+1) ) / 2;
+                bit = decimal % 2;
+
+                //division =  arrayPatron[j+1] / 2;
+                //division =  arrayPatron[m] / 2;
+                //cout << "-*" << division;
+                //cout << "-*" << bit;
+                //bit = *(arrayPatron + (j+1) ) % 2;
+                //bit = arrayPatron[j+1] % 2;
+
+                //*(*(patron+j)+k) = bit;
+                patron[j][k] = bit;
+                //cout << "-" << bit;
+                //m++;
+
+                if( decimal / 2 == 0 ){
+                    biteDivide = false;
+                }
+
+                decimal = decimal / 2;
+            }
+            //cout << "K: " << k << endl;
+            for(int m=k-1; m > 0 ; m-- ){
+                patron[j][m] = 0;
+                // *(*(patron+j)+k) = 0;
+            }
+            //cout << "-" << bit;
+        //} //END for k
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+int verificacion(int matriz[][8]){
+
+    // Rellenado de toda la matríz con valores igual a 1
+    for(int row = 0; row < MAXROW ; row++){
+        for(int column = 0; column < MAXCOLUMN ; column++){
+            matriz[row][column] = 1;
+            cout << " " << matriz[row][column];
+            //dataSER(matriz[row][column]);
+            signalSRCLK();
+            signalRCLK();
+        }
+        //cout << endl;
+    }
 
     //dataSER(matriz[]);
 
@@ -105,7 +248,7 @@ int verificacion(int matriz[][8]){
 }
 
 //******************************************************************
-int imagen(int charA[][8], int matriz[][8]){
+int imagen(int patron[][8], int matriz[][8]){
     /*for(int row = 0; row < MAXROW ; row++){
       dataSER(charA[row]);
       signalSRCLK();
@@ -114,15 +257,15 @@ int imagen(int charA[][8], int matriz[][8]){
 
     for(int row = 0; row < MAXROW ; row++){
       for(int column = 0; column < MAXCOLUMN ; column++){
-        matriz[row][column] = charA[row][column];
-        //cout << " " << matriz[row][column];
+        matriz[row][column] = patron[row][column];
+        cout << " " << matriz[row][column];
         dataSER(matriz[row][column]);
         signalSRCLK();
         signalRCLK();
       }
-      //cout << endl;
+      cout << endl;
     }
-
+    return 0;
 }
 
 
